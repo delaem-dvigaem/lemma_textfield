98597f746331aa399bdd6e217b6aa2571ee8271d
delaem_dvigaem <lead@delaem-dvigaem.ru>
2022-07-14 11:10:25 +0300

    Добавил каретку и ее анимацию

--- lib/lemma_textfield.dart
+++ lib/lemma_textfield.dart
@@ -1,7 +1,76 @@
-library lemma_textfield;
+library flutter_pkg;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+import 'caret.dart';
+
+class LemmaTextField extends StatefulWidget {
+  var placeholder;
+
+  LemmaTextField({Key? key, this.placeholder}) : super(key: key);
+
+  @override
+  State<LemmaTextField> createState() => _LemmaTextFieldState();
+}
+
+class _LemmaTextFieldState extends State<LemmaTextField> {
+  late bool _caretVisibility = false;
+  final _focusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode.addListener(() {});
+  }
+
+  void showCaret() {
+    if (mounted) {
+      setState(() => _caretVisibility = true);
+    }
+  }
+
+  void hideCaret() {
+    if (mounted) {
+      setState(() => _caretVisibility = false);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+        cursor: SystemMouseCursors.text,
+        child: InkWell(
+          focusNode: _focusNode,
+          onFocusChange: (hasFocus) {
+            hasFocus ? showCaret() : hideCaret();
+          },
+          child: Container(
+            padding: const EdgeInsets.all(10.0),
+            decoration:
+                BoxDecoration(border: Border.all(color: Colors.blueAccent)),
+            child: Stack(
+              children: [
+                Text(
+                  widget.placeholder,
+                  style: TextStyle(
+                      color: Color.fromRGBO(191, 191, 191, 1), fontSize: 18),
+                ),
+                Visibility(
+                  child: Caret(),
+                  visible: _caretVisibility,
+                )
+              ],
+            ),
+          ),
+          onTap: () {
+            _focusNode.requestFocus();
+          },
+        ));
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    super.dispose();
+  }
 }
